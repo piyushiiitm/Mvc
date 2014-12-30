a3b54402d36b3f6d43b749a414420975c83335b3
piyushiiitm <piyushiiitm@gmail.com>
2014-12-30 16:25:06 +0530

    test

---
+++ test/image.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<form method="post" action="/user/update?access_token=d8948f16bc3363a4fa6d734cb5922c73" enctype="multipart/form-data">
+
+
+
+<input name="profile_pic" type="file">
+<input type="submit">
+
+</form>
